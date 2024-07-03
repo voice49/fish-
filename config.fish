e6dbbbe298f765c34109aa02ce4fa7afe1aa7ccc
voice49 <55628323+voice49@users.noreply.github.com>
2024-07-03 22:03:41 +0300

    Add files via upload

--- config.fish
+++ config.fish
@@ -6,7 +6,8 @@ alias ls='exa -al --color=always --group-directories-first' # my preferred listi
 alias la='exa -a --color=always --group-directories-first'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first'  # long format
 alias lt='exa -aT --color=always --group-directories-first' # tree listing
-alias l.='exa -a | egrep "^\."'
+alias l='exa -a | egrep "^\."'
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
 
 ### "bat" as manpager
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
@@ -16,7 +17,6 @@ export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
 alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
 alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
-
-
-set fish_greeting ""
+set -g fish_greeting
 eval (starship init fish)
+zoxide init  --cmd cd fish | source
